2a0ff756539b435a627ed2f6a78aa3aee353200b
Sparsh Shandilya <sparshshandilya123@gmail.com>
2024-04-19 16:08:07 +0530

    started loader

---
+++ src/components/Created/Loader.jsx
@@ -0,0 +1,11 @@
+import loader from "../../assets/loader.svg";
+
+const Loader = () => {
+  return (
+    <>
+      <img src={loader} alt="loader" className="w-[100px]" />
+    </>
+  );
+};
+
+export default Loader;
